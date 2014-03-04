f27d741d9e4cddb9259fc96b553bd186e03ceb80
Tyler Stowell <tyler.d.stowell@gmail.com>
2014-03-04 01:29:13 -0500

    Last for the night

--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class movementController : MonoBehaviour {
 
 	Animator a;
-	float speed;
+	float speed=2f;
 
 	
 	void Start () {
@@ -12,20 +12,21 @@ public class movementController : MonoBehaviour {
 	}
 
 	void Update () {
+		float pos = transform.position.x;
 		if (Input.GetKey ("left")) {
-			speed = -2;
+			pos-=speed*Time.deltaTime;
 			a.SetBool("onRight",true);
 			if(transform.rotation != Quaternion.Euler(0, 180, 0)) transform.rotation = Quaternion.Euler(0, 180, 0);
 		}
 		else a.SetBool("onRight",false);
 		if (Input.GetKey ("right")) {
+			pos+=speed*Time.deltaTime;
 			a.SetBool("onRight",true);
-			speed = 2;
 			if(transform.rotation != Quaternion.Euler(0, 0, 0)) transform.rotation = Quaternion.Euler(0, 0, 0);
 		}
 		else a.SetBool("onRight",false);
 		float translation = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
-		transform.position = new Vector3(transform.position.x + translation, transform.position.y, transform.position.z);
+		transform.position = new Vector3(pos, transform.position.y, transform.position.z);
 	}
 
 
