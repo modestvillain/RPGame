9a520a1d173dfd63890a8437390f51d40916e8f1
Tyler Stowell <tyler.d.stowell@gmail.com>
2014-03-04 00:47:10 -0500

    Trying to fix controller

--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -3,13 +3,28 @@ using System.Collections;
 
 public class movementController : MonoBehaviour {
 
-	// Use this for initialization
+	Animator a;
+	float speed = 2;
+
+	
 	void Start () {
-	
+		a = GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		if (Input.GetKey ("left")) {
+			a.SetBool("onRight",true);
+			if(transform.rotation != Quaternion.Euler(0, 180, 0)) transform.rotation = Quaternion.Euler(0, 180, 0);
+		}
+		else a.SetBool("onLeft",false);
+		if (Input.GetKey ("right")) {
+			a.SetBool("onRight",true);
+			if(transform.rotation != Quaternion.Euler(0, 0, 0)) transform.rotation = Quaternion.Euler(0, 0, 0);
+		}
+		else a.SetBool("onRight",false);
+		float translation = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (translation , 0, 0);
 	}
+
+
 }
