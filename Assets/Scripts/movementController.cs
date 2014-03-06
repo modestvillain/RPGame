ed4a121b93ced4027199ae16bf350dae49ff205a
Tyler Stowell <tyler.d.stowell@gmail.com>
2014-03-06 00:24:29 -0500

    Fixed/hacked turning

--- Assets/Scripts/movementController.cs
+++ Assets/Scripts/movementController.cs
@@ -5,14 +5,14 @@ public class movementController : MonoBehaviour {
 
 	Animator a;
 	CharacterController c;
+	GameObject player;
 	float speed=2f;
-	bool facingRight=true,
-		 facingLeft=false;
+	bool facingRight=true;
 
-	
 	void Start () {
 		a = GetComponent<Animator> ();
 		c = GetComponent<CharacterController> ();
+		player = c.gameObject;
 	}
 
 	void Update () {
@@ -20,35 +20,42 @@ public class movementController : MonoBehaviour {
 		float posy = transform.position.y;
 		if (Input.GetKey ("left")) {
 			posx-=speed*Time.deltaTime;
-			a.SetBool("onRight",true);
+
 			if(facingRight)	{
-				Debug.Log ("run");
-				Vector3 v = transform.localScale;
-				v.x *= -1;
-				transform.localScale = v;
-				facingRight=false;
-				facingLeft=true;
+				gameObject.SetActive(false);
+				Flip (player);
+				Debug.Log (transform.localScale + "LEFT");
 			}
+			a.SetBool("onRight",true);
+			gameObject.SetActive(true);
 		}
-		//else a.SetBool("onRight",false);
 		else if (Input.GetKey ("right")) {
 			posx+=speed*Time.deltaTime;
-			a.SetBool("onRight",true);
-			if(facingLeft)	{
-				Vector3 v = transform.localScale;
-				v.x *= -1;
-				transform.localScale = v;
-				facingLeft=false;
-				facingRight=true;
+
+			if(!facingRight) {
+				Flip (player);
+				Debug.Log (transform.localScale + "RIGHT");
 			}
+			a.SetBool("onRight",true);
 		}
 		else a.SetBool("onRight",false);
-		float x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		c.SimpleMove(Physics.gravity);
+		//float x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
 		//posy -= 9.81f * Time.deltaTime;
 		//c.SimpleMove(new Vector3(1, 1, 0) );
 		//transform.position = new Vector3(pos, transform.position.y, transform.position.z);
 		if(c.isGrounded) Debug.Log("yeah");
 	}
 
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Debug.Log ("collided");
+		c.SimpleMove(new Vector3(0,0,0) );
+	}
 
+	void Flip(GameObject g) {
+		Vector3 v = g.transform.localScale;
+		v.x *= -1;
+		g.transform.localScale = v;
+		facingRight=!facingRight;
+	}
 }
